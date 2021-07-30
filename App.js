b3c0689d0405d8dd7f9617d7b874ce29e98a2c9f
tm-j-chn <timchun0212@gmail.com>
2021-07-30 10:33:21 +0900

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, TextInput, View, Button } from "react-native";
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class extends React.Component {
+  state = {
+    text: "",
+  };
+
+  async sendData(newText) {
+    await axios
+      .post("https://got-todo-app.herokuapp.com/post/send", {
+        text: newText,
+      })
+      .then(
+        (res) => {
+          console.log(res.status);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    // await axios.get("https://got-todo-app.herokuapp.com/post/send").then(
+    //   (res) => {
+    //     console.log(res.status);
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   }
+    // );
+  }
+
+  componentDidMount() {
+    this.textInput = React.createRef();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.text}
+          placeholder="Enter you text!"
+          textAlign="center"
+          onChangeText={(newText) => this.setState({ text: newText })}
+          ref={(input) => {
+            this.textInput = input;
+          }}
+        />
+        <Button
+          title="Send"
+          onPress={() => {
+            this.sendData(this.state["text"]);
+            this.textInput.clear();
+          }}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    height: 40,
   },
 });
---
+++ Text.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+
+const styles = StyleSheet.create({});
